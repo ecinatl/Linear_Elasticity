cd6c16a7ac8437c6e0a67ab8bfe18db1f56e37fd
Emma Cinatl <mathgirl1024@gmail.com>
2018-11-23 16:18:44 -0500

    updated the readme and some copyright info

--- displacement_pressure_timoshenko/disp_pressure_timoshenko.cc
+++ displacement_pressure_timoshenko/disp_pressure_timoshenko.cc
@@ -1,22 +1,13 @@
 /* ---------------------------------------------------------------------
 *
-* Copyright (C) 2000 - 2016 by the deal.II authors
+* Copyright (C) 2018
 *
-* This file is part of the deal.II library.
-*
-* The deal.II library is free software; you can use it, redistribute
-* it, and/or modify it under the terms of the GNU Lesser General
-* Public License as published by the Free Software Foundation; either
-* version 2.1 of the License, or (at your option) any later version.
-* The full text of the license can be found in the file LICENSE at
-* the top level of the deal.II distribution.
+* Author: Emma Cinatl, Clemson University, 2018
+* Based on the deal.II step-8 tutorial program
 *
 * ---------------------------------------------------------------------
-
 *
-* Author: Wolfgang Bangerth, University of Heidelberg, 2000
 */
-
 
 
 #include <deal.II/base/quadrature_lib.h>
